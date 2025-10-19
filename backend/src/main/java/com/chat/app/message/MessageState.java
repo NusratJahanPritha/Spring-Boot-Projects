d7942ee33eb9d8cc9f247a1d3b008228dca0ffc4
Nusrat Jahan <nusrat.ruet.ece15@gmail.com>
2025-10-19 14:11:30 +0600

    Final project upload

---
+++ backend/src/main/java/com/chat/app/message/MessageState.java
@@ -0,0 +1,7 @@
+package com.chat.app.message;
+
+public enum MessageState {
+
+    SENT,
+    SEEN
+}
---
+++ backend/src/main/java/com/chat/app/notification/NotificationType.java
@@ -0,0 +1,11 @@
+package com.chat.app.notification;
+
+public enum NotificationType {
+
+    SEEN,
+    MESSAGE,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+
+}
